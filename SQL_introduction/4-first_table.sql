1bce573ca245f27a15c8e7becf13301bd7adc771
danrealb <danrealb>
2023-08-15 05:23:57 -0700

    creating tables in description

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -6,7 +6,7 @@
 -- if the table first_table already exists, your scrip should not fall
 -- you are not allowed to use the SELECT or SHOW statements 
 
-CREATE TABLE IF EXISTS first_table(
+CREATE TABLE IF NOT EXISTS first_table(
 	id INT,
 	name VARCHAR(256),
 );
