059842a4f6ff4d7734bfb81f52e5252517964969
danrealb <danrealb>
2023-08-25 09:34:16 -0700

    create user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- creates the MySQL server user user_0d_1.
+-- user_0d_1 password should be set to user_0d_pwd
+-- if the user user_0d_1 already exists, your should not fail
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENFITIED BY 'user_0d_pwd';
+
