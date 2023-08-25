f6d80a86ce071a720aedb1df2089fb33cdab5b5e
danrealb <danrealb>
2023-08-25 09:56:51 -0700

    create user with privilege

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,6 +3,6 @@
 -- user_0d_1 password should be set to user_0d_pwd
 -- if the user user_0d_1 already exists, your should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENFITIED BY 'user_0d_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENFITIED BY 'password';
 
 GRANT ALL PRIVILEGES ON *.* 'user_0d_1'@'localhost';
