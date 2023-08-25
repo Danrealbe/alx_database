a79ab4d25a00c49d1dc169a34fe98be47ffea3b9
danrealb <danrealb>
2023-08-25 09:38:45 -0700

    create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,8 @@
--- creates the MySQL server user user_0d_1.
+-- creates the MySQL server user user_0d_1.i
+-- user_0d_1 should have all privileges on your MySQL server
 -- user_0d_1 password should be set to user_0d_pwd
 -- if the user user_0d_1 already exists, your should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENFITIED BY 'user_0d_pwd';
 
+GRANT ALL PRIVILEGES ON *.* 'user_0d_1'@'localhost';
