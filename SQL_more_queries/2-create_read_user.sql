f8f4505b4d5de516e0c9b3e982625e34572d4a85
danrealb <danrealb>
2023-08-26 06:16:19 -0700

    creating database/user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,7 +6,10 @@
 
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT, SHOW VIEW ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
+
+GRANT 'user_0d_2@localhost';
+GRANT USAGE ON *.* TO 'user_0d_2`@`localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2`@`localhost';
 FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS 'hbt_0d_2';
